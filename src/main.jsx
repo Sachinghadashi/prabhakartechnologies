a0bfefc370303428e8d96a5ff0941aa7b1d6d671
Sachinghadashi <sachinghadashi56@gmail.com>
2026-02-05 18:29:45 +0530

    Fix HashRouter closing tag

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 
 import App from "./App";
@@ -8,8 +8,8 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <HelmetProvider>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </HelmetProvider>
 );
