9417236256149a84ba1fea98c0122dc1867fd0b2
Sachinghadashi <sachinghadashi56@gmail.com>
2026-02-05 13:00:50 +0530

    Add responsive navbar and mobile UI fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,50 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <nav className="navbar">
-      <h2>Prabhakar Technologies</h2>
 
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/services">Services</Link></li>
-        <li><Link to="/careers">Careers</Link></li>
-        <li><Link to="/clients">Clients</Link></li>
+      <div className="nav-logo">
+        Prabhakar Technologies
+      </div>
+
+      {/* Hamburger */}
+      <div
+        className="hamburger"
+        onClick={() => setOpen(!open)}
+      >
+        ☰
+      </div>
+
+      {/* Menu */}
+      <ul className={open ? "nav-menu active" : "nav-menu"}>
+
+        <li onClick={() => setOpen(false)}>
+          <Link to="/">Home</Link>
+        </li>
+
+        <li onClick={() => setOpen(false)}>
+          <Link to="/about">About</Link>
+        </li>
+
+        <li onClick={() => setOpen(false)}>
+          <Link to="/services">Services</Link>
+        </li>
+
+        <li onClick={() => setOpen(false)}>
+          <Link to="/careers">Careers</Link>
+        </li>
+
+        <li onClick={() => setOpen(false)}>
+          <Link to="/clients">Clients</Link>
+        </li>
+
       </ul>
+
     </nav>
   );
 };
