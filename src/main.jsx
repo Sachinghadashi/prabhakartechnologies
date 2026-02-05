b37ecd0a1ab81113ff4a8c44493c47783864600c
Sachinghadashi <sachinghadashi56@gmail.com>
2026-02-05 18:46:59 +0530

    Remove dist from repo and fix SPA rewrites

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 
 import App from "./App";
@@ -8,8 +8,8 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <HelmetProvider>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </HelmetProvider>
 );
